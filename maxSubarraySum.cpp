fe25e63035e2869da04a6185128ca16461ec16a9
Kajal Shakya <kajal.kshakya@gmail.com>
2025-09-22 00:59:00 +0530

    print maximum subarray sum by bruteforce method

---
+++ maxSubarraySum.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+void maxSubarraySum(int arr[],int n){
+    int maxSum=INT16_MIN;
+    for(int start=0;start<n;start++){
+        for(int end=start;end<n;end++){
+            int currSum=0;
+            for(int i=start;i<=end;i++){
+            currSum+=arr[i];
+        }
+        cout<<currSum<<",";
+        maxSum=max(maxSum,currSum);
+        }
+        cout<<endl;
+        
+    }
+    cout<<" maximum sum :"<<maxSum;
+}
+int main(){
+    int arr[6]={2,-3,6,-5,4,2};
+    int n=sizeof(arr)/sizeof(int);
+    maxSubarraySum(arr,n);
+
+    return 0;
+}
