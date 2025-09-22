7460bb3bdee1dfc1f6ebeff326d89e910656ee31
Kajal Shakya <kajal.kshakya@gmail.com>
2025-09-23 02:17:20 +0530

    optimize hard problem - Trapping Rain Water

--- trappingRainWaterProblem.cpp
+++ trappingRainWaterProblem.cpp
@@ -10,15 +10,15 @@ int trap(int height[],int n){
     for(int i=n-2;i>=0;i--){
         rightMax[i]=max(rightMax[i+1],height[i+1]);
          }
-    int trapedWater=0;
+    int trappedWater=0;
     for(int i=0;i<n;i++){
         int currWater=min(leftMax[i],rightMax[i])-height[i];
         if(currWater>0){
-            trapedWater+=currWater;
+            trappedWater+=currWater;
         }
         
-    }cout<<" total traped water :";
-    return trapedWater;
+    }cout<<" total trapped water :";
+    return trappedWater;
         
     }
 
