91adccf51a1a2babf4aca7c7aea204f8f531fe72
Kajal Shakya <kajal.kshakya@gmail.com>
2025-09-23 13:52:12 +0530

    Implement containsDuplicate function for vector

---
+++ containsDuplicate.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+bool containsDuplicate(vector<int>&nums){
+    sort(nums.begin(),nums.end());
+    for(int i=1;i<nums.size();i++){
+        if(nums[i]==nums[i-1]){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+int main(){
+    vector<int>nums={1,1,1,3,3,4,3,2,4,2};
+    if(containsDuplicate(nums)){
+        cout<<" Duplicate Exist "<<endl;
+    }
+    else{
+        cout<<"All elements are unique "<<endl;
+    }
+    return 0;
+}
