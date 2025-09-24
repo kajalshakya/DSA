16e4f731a022254ae717034ea01a90e7cd6baebc
Kajal Shakya <kajal.kshakya@gmail.com>
2025-09-24 10:52:54 +0530

    feat: implement contains duplicate check using hashset

---
+++ containsduplicate.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool search(vector<int> &nums){
+    unordered_set<int>set;
+    for(int i=0;i<nums.size();i++){
+        if(set.find(nums[i])==set.end()){
+            set.insert(nums[i]);
+        }
+        else{ 
+            return true;
+        }
+    }
+    return false;
+}
+int main(){
+    vector<int>nums={1,2};
+   if(search(nums)){
+    cout<<" There exist a duplicate "<<endl;
+   }
+   else{
+        cout<<" All elements are unique "<<endl;
+   }
+
+    return 0;
+}
