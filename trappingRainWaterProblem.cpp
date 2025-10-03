befa11b806de59469f4ee1e1182014b6e0aa8fe0
Kajal Shakya <kajal.kshakya@gmail.com>
2025-10-04 00:45:30 +0530

    add a solution to find a target sum by adding any two elements of an  array

--- trappingRainWaterProblem.cpp
+++ trappingRainWaterProblem.cpp
@@ -5,10 +5,10 @@ int trap(int height[],int n){
     leftMax[0]=height[0];
     rightMax[n-1]=height[n-1];
     for(int i=1;i<n;i++){
-        leftMax[i]=max(leftMax[i-1],height[i-1]);
+        leftMax[i]=max(leftMax[i-1],height[i]);
         }
     for(int i=n-2;i>=0;i--){
-        rightMax[i]=max(rightMax[i+1],height[i+1]);
+        rightMax[i]=max(rightMax[i+1],height[i]);
          }
     int trappedWater=0;
     for(int i=0;i<n;i++){
