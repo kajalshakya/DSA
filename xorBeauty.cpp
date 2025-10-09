77871387640e7df003b65d8d0178ad884dfe24e5
Kajal Shakya <kajal.kshakya@gmail.com>
2025-10-09 23:44:01 +0530

    Implement XOR Beauty problem solution

---
+++ xorBeauty.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+   int xorBeauty(vector<int>& nums) {
+        int ans = 0;
+        for (int num : nums) {
+            ans ^= num;  // XOR all elements
+        }
+        return ans;
+    }
+
+
+int main() {
+   
+    vector<int> nums = {1, 2, 3};
+    cout << "XOR Beauty: " <<xorBeauty(nums) << endl;
+    return 0;
+}
