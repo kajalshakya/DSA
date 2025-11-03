8dd54694d079796a315eecd255696f1923633c75
Kajal Shakya <kajal.kshakya@gmail.com>
2025-11-03 23:55:06 +0530

    implement Unique Paths (m x n grid) using combinatorics formula

---
+++ gridWaysVariation.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int uniquePaths(int m,int n){
+    int res=1;
+    int N=m+n-2;
+    int r=min(m-1,n-1);
+    for(int i=1;i<=r;i++){
+        res=res*(N-r+i)/i;
+    }return res;
+}
+int main(){
+    cout<<uniquePaths(3,7);
+    return 0;
+}
