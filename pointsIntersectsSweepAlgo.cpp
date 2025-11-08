7408e7033dddb5e1a333e2a6968b36bd3f5e5688
Kajal Shakya <kajal.kshakya@gmail.com>
2025-11-08 23:35:49 +0530

    Implement solution for LeetCode 2848 using unordered_set to track unique covered points

---
+++ pointsIntersectsSweepAlgo.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+//time complexity:O(N);
+//space complexity:O(1)
+int numberOfPoints(vector<vector<int>>& nums){
+    unordered_set<int>s;
+    for(auto &i:nums){
+        for(int j=i[0];j<=i[1];j++){
+            s.insert(j);
+        }
+    }
+    return s.size();
+}
+
+int main() {
+    vector<vector<int>>nums = {{3,6},{1,5},{4,7}};
+    cout<<"total poits:"<<numberOfPoints(nums);
+    return 0;
+}
+
+
+    
+    
+
