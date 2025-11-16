2574b6caaf9fb761c910e80f9a4cace554ad89a6
Kajal Shakya <kajal.kshakya@gmail.com>
2025-11-16 23:54:18 +0530

    Implement LeetCode 70 - Climbing Stairs using space-optimized DP

---
+++ climbingStairs.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int climbStairs(int n) {
+    if (n <= 2) return n;
+    int a = 1, b = 2;
+    for (int i = 3; i <= n; i++) {
+        int c = a + b;
+        a = b;
+        b = c;
+    }
+        return b;
+    }
+
+int main(){
+    cout<<"ans:"<<climbStairs(3);
+    return 0;
+}
+
