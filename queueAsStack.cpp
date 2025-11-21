06f394bea6a14bf4ff7b26337b0790d2e91300f1
Kajal Shakya <kajal.kshakya@gmail.com>
2025-11-21 22:54:57 +0530

    Implement queue as stack

---
+++ queueAsStack.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class QueueUsingStacks {
+    stack<int> s1, s2;
+
+public:
+    void enqueue(int x) {
+        s1.push(x);
+    }
+
+    int dequeue() {
+        if (s1.empty() && s2.empty()) {
+            cout << "Queue is empty\n";
+            return -1;
+        }
+
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        int val = s2.top();
+        s2.pop();
+        return val;
+    }
+
+    int peek() {
+        if (s1.empty() && s2.empty()) {
+            cout << "Queue is empty\n";
+            return -1;
+        }
+
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        return s2.top();
+    }
+
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+};
+
+int main() {
+    QueueUsingStacks q;
+
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+
+    cout << "Dequeue: " << q.dequeue() << endl; 
+    cout << "Peek: " << q.peek() << endl;       
+
+    q.enqueue(40);
+    cout << "Dequeue: " << q.dequeue() << endl; 
+    cout << "Dequeue: " << q.dequeue() << endl; 
+    cout << "Dequeue: " << q.dequeue() << endl;  
+    cout << "Dequeue: " << q.dequeue() << endl;  
+
+    return 0;
+}
