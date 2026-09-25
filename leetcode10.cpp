91626cbc51f07d6b4f318b6221cf4935fdbd1b96
Kajal Shakya <kajal.kshakya@gmail.com>
2026-09-25 12:23:34 +0530

    Add LeetCode 10 solution

---
+++ leetcode10.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+
+ListNode* rotateRight(ListNode* head, int k) {
+
+    if (head == NULL || head->next == NULL || k == 0)
+        return head;
+
+    int n = 1;
+    ListNode* tail = head;
+
+    // Find length and last node
+    while (tail->next != NULL) {
+        tail = tail->next;
+        n++;
+    }
+
+    k = k % n;
+
+    if (k == 0)
+        return head;
+
+    // Find new tail
+    int steps = n - k;
+    ListNode* newTail = head;
+
+    for (int i = 1; i < steps; i++) {
+        newTail = newTail->next;
+    }
+
+    // New head
+    ListNode* newHead = newTail->next;
+
+    // Break the list
+    newTail->next = NULL;
+
+    // Connect old tail to old head
+    tail->next = head;
+
+    return newHead;
+}
+
+void printList(ListNode* head) {
+
+    while (head != NULL) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+
+    cout << endl;
+}
+
+int main() {
+
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    int k = 2;
+
+    cout << "Original List: ";
+    printList(head);
+
+    head = rotateRight(head, k);
+
+    cout << "Rotated List: ";
+    printList(head);
+
+    return 0;
+}
