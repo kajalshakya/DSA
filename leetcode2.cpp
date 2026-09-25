3336420d148ca5b75840c3df4fa2c858b323eee8
Kajal Shakya <kajal.kshakya@gmail.com>
2026-09-25 12:22:09 +0530

    Add LeetCode 2 solution

---
+++ leetcode2.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+    ListNode* dummy = new ListNode(0);
+    ListNode* curr = dummy;
+
+    int carry = 0;
+
+    while (l1 != NULL || l2 != NULL || carry != 0) {
+
+        int sum = carry;
+
+        if (l1 != NULL) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+
+        if (l2 != NULL) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+
+        carry = sum / 10;
+
+        curr->next = new ListNode(sum % 10);
+        curr = curr->next;
+    }
+
+    return dummy->next;
+}
+
+void printList(ListNode* head) {
+
+    while (head != NULL) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+
+    cout << endl;
+}
+
+int main() {
+
+    // 342 = 2 -> 4 -> 3
+    ListNode* l1 = new ListNode(2);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(3);
+
+    // 465 = 5 -> 6 -> 4
+    ListNode* l2 = new ListNode(5);
+    l2->next = new ListNode(6);
+    l2->next->next = new ListNode(4);
+
+    ListNode* result = addTwoNumbers(l1, l2);
+
+    cout << "Result: ";
+    printList(result);
+
+    return 0;
+}
